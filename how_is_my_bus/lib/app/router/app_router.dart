afc165677114ac7453e02a121dce7d6e6f8ca491
seba <komorowski.seba@gmail.com>
2026-08-19 23:49:47 +0200

    route

---
+++ how_is_my_bus/lib/app/router/app_router.dart
@@ -0,0 +1,4 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/settings/presentation/pages/settings_page.dart';

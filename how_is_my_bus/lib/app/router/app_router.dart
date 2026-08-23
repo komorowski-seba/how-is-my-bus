3c425c90b9d73824a84a89e23c021dac6a83ae61
seba <komorowski.seba@gmail.com>
2026-08-23 13:25:04 +0200

    changes

--- how_is_my_bus/lib/app/router/app_router.dart
+++ how_is_my_bus/lib/app/router/app_router.dart
@@ -2,3 +2,21 @@ import 'package:go_router/go_router.dart';
 
 import '../../features/home/presentation/pages/home_page.dart';
 import '../../features/settings/presentation/pages/settings_page.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/home',
+      builder: (context, state) {
+        return const HomePage();
+      },
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (context, state) {
+        return const SettingsPage();
+      },
+    ),
+  ],
+);
---
+++ how_is_my_bus/lib/app/widgets/app_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class const DrawerApp({super.key}) extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final location = GoRouterState.of(context).uri.path;
+
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            const DrawerHeader(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.flutter_dash,
+                      size: 50,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Hello hi',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                )
+            ),
+
+            ListTile(
+              selected: location == '/home',
+              leading: const Icon(Icons.home),
+              title: const Text('home'),
+              onTap: () {
+                context.go('/home');
+              },
+            ),
+
+            ListTile(
+              selected: location == '/settings',
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                context.go('/settings');
+              },
+            )
+          ],
+        )
+      )
+    );
+  }
+
+}

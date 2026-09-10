b77dcb9852aa5dc9a72a47fce7885a6393daf268
seba <komorowski.seba@gmail.com>
2026-09-10 16:00:46 +0200

    changes

--- how_is_my_bus/lib/app/view/home_page.dart
+++ how_is_my_bus/lib/app/view/home_page.dart
@@ -32,8 +32,6 @@ class HomePage extends StatelessWidget implements PageName {
 }
 
 // protobud (grpc) https://cdn.zbiorkom.live/
-// mkuran.pl: Highly regarded as the most reliable community source for Poland. It provides cleaned GTFS and GTFS-RT (Real-Time) feeds for Warsaw (ZTM), Polish Trains (PKP/Polregio), and several smaller cities.
-// •
 // poland.transport.rest: A developer-friendly REST API. It's great if you want to avoid parsing complex Protobuf (.pb) files yourself, as it returns JSON.
 // •
 // Mobility Database: A global registry that mirrors many official Polish feeds (Kraków, Poznań, etc.).
--- how_is_my_bus/lib/app/widgets/user_tracking_map.dart
+++ how_is_my_bus/lib/app/widgets/user_tracking_map.dart
@@ -42,10 +42,8 @@ class UserTrackingMap extends ConsumerWidget {
             ),
             children: [
               TileLayer(
-                urlTemplate:
-                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName:
-                'com.example.mytrackingapp',
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.mytrackingapp',
               ),
 
               MarkerLayer(
